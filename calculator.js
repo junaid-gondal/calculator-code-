83f17dad1f4226044ceb2c9faf533ec90a6b5769
Junaid Gondal <gondalj646@gmail.com>
2026-04-26 20:49:25 +0500

    Add java code
    Implement basic calculator functionality with input validation.

---
+++ calculator.js
@@ -0,0 +1,52 @@
+
+let input = document.getElementById("inputbox");
+let buttons = document.querySelectorAll('button');
+let string = "";
+let arr = Array.from(buttons);
+
+arr.forEach(button =>
+     {
+    button.addEventListener('click', (e) => {
+        
+        if (e.target.innerHTML == '=') 
+        {       
+            string = eval(string);
+            input.value= string;
+        } 
+        else if (e.target.innerHTML == 'C') 
+        { 
+            string = "";
+            input.value = string;
+        } 
+        else
+        { 
+            
+            string += e.target.innerHTML;  
+            
+            if(string[0]=='%' )
+                {
+                    alert("wrong input");
+                    string="";
+                }   
+            else if(string[0]=='+' )
+            {
+                    alert("wrong input");
+                    string="";
+            }  
+          else if(string[0]=='-' )
+            {
+                    alert("wrong input");
+                    string="";
+            }  
+          else if(string[0]=='/' )
+            {
+                    alert("wrong input");
+                    string="";
+            }   
+            else{
+                input.value= string;
+            }  
+            
+        }
+    });
+});
